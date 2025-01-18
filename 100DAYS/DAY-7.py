4dc44b9359373134c3995bb2611164609b84a73c
JumbeeDEV <112726359+JumbeeDEV@users.noreply.github.com>
2025-01-18 23:12:13 +0300

    Update DAY-7.py

--- 100DAYS/DAY-7.py
+++ 100DAYS/DAY-7.py
@@ -1,18 +1,27 @@
 import random
 
+end_of_game = False
+
 wordlist = ["istanbul", "paris", "tokyo"]
-
 chosen_word = random.choice(wordlist)
-
 print(chosen_word)
 
-for char in chosen_word:
-    print("_" , end=" ")
+display = []
+while not end_of_game:
+    user_guess = input("Guess a letter: ").lower()
 
-user_guess = input("Guess a letter: ").lower()
-for letter in chosen_word:
-    if letter == user_guess:
-        print(letter, end = " ")
-    elif letter != user_guess:
-        print("_", end = " ") 
-    
+    for letter in chosen_word:
+        if letter == user_guess:
+            display.append(letter)
+        elif letter != user_guess:
+            display += "_"
+
+    print(display)
+
+
+
+if "_" not in display:
+    end_of_game = True
+    print("You win")
+
+
