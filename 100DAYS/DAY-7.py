c8a06b1f49390df5d4d435d0d27f4bb8992d727e
JumbeeDEV <112726359+JumbeeDEV@users.noreply.github.com>
2025-01-19 00:03:06 +0300

    Update DAY-7.py

--- 100DAYS/DAY-7.py
+++ 100DAYS/DAY-7.py
@@ -6,12 +6,16 @@ wordlist = ["istanbul", "paris", "tokyo"]
 chosen_word = random.choice(wordlist)
 print(chosen_word)
 
-display = []
+display = ["_" for _ in chosen_word]
+guessed_letters = set()
+
 while not end_of_game:
-    for char in chosen_word:
-        display += "_"
+    print(display)
     user_guess = input("Guess a letter: ").lower()
-    
+    if user_guess in guessed_letters:
+        print("you already guessed this letter")
+        continue
+    guessed_letters.add(user_guess)
 
     for position in range(len(chosen_word)):
         letter = chosen_word[position]
@@ -21,6 +25,7 @@ while not end_of_game:
     if "_" not in display:
         end_of_game = True
         print("You win")
+        print(display)
     else:
         print("try again")
     
